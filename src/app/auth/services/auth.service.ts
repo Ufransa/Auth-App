eb991118b8d4d2ddc626be1673180b380d0965ca
Francisco Segura <fjsc664@gmail.com>
2023-06-18 14:43:22 +0200

    Finish project

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -51,7 +51,10 @@ export class AuthService {
     const url = `${this.baseUrl}/auth/check-token`
     const token = localStorage.getItem('token')
 
-    if (!token) of(false)
+    if (!token) {
+      this.logout()
+      return of(false)
+    }
 
     const headers = new HttpHeaders()
       .set('Authorization', `Bearer ${token}`)
